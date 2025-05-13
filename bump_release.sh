9d2b582dd0bdd6b264de1986f2cf186139542f32
LaaniLoo <alain.x.mackay@gmail.com>
2025-05-13 16:05:08 +1000

    bump: version 0.4.1 → 0.4.2

--- bump_release.sh
+++ bump_release.sh
@@ -3,6 +3,15 @@ bump_release() {
     read -p "Bump and push? [y = yes, n = no]: " push
 
     if [[ "$push" == "y" ]]; then
+
+        next_version=$(cz bump --get-next)
+        echo "Next version will be: v$next_version"
+        git-cliff --tag $next_version -o CHANGELOG.md
+
+        git add CHANGELOG.md
+        git commit -m "docs: update changelog"
+
+
         cz bump
         git tag
     fi
