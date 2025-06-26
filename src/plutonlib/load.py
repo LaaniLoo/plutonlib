f7fc11b57a508f2e99975196e6ec5879b033d4f1
LaaniLoo <alain.x.mackay@gmail.com>
2025-06-26 19:42:43 +1000

    test: attempt to get plot.py to work on cluster

--- src/plutonlib/load.py
+++ src/plutonlib/load.py
@@ -637,7 +637,9 @@ def pluto_loader(sim_type, run_name, profile_choice,load_outputs=None):
         vars[f"data_{output_num}"] = file_data
 
         #DEBUG
-        warnings.append(f"{pcolours.WARNING}loaded data_{output_num}")
+        # warnings.append(f"{pcolours.WARNING}loaded data_{output_num}")
+        print(f"{pcolours.WARNING}loaded data_{output_num}")
+
 
     var_choice = [v for v in var_choice if v not in non_vars]
     vars_extra.append(geometry) # gets the geo of the sim, always loads first file
