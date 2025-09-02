8ec880c59b9b263a6def574dd0c46890cc02180b
LaaniLoo <alain.x.mackay@gmail.com>
2025-09-02 16:18:21 +1000

    fix(config.py): replaced simple `profiles` dict with function to get correct profiles/array types
    this includes the merging of `profiles` and `profiles2()`

--- src/plutonlib/config.py
+++ src/plutonlib/config.py
@@ -33,18 +33,6 @@ if os.path.isdir(os.path.join(plutodir, "Simulations")): #if simulation dir does
 else:
     raise FileNotFoundError(f"{pcolours.WARNING} Simulation directory not found, needs to be in PLUTO_DIR ({plutodir}), see sim_save.sh")
 
-# profiles = {
-#     "all": ["x1", "x2", "x3", "rho", "prs", "vx1", "vx2", "vx3", "sim_time"],
-#     "grid_time": ["x1","x2","x3","sim_time"],
-#     "xy_rho_prs": ["x1", "x2", "rho", "prs"],
-#     "xz_rho_prs": ["x1", "x3", "rho", "prs"],
-#     "yz_rho_prs": ["x2", "x3", "rho", "prs"],
-
-#     "xy_vel": ["x1", "x2", "vx1", "vx2"],
-#     "xz_vel": ['x1','x3','vx1','vx3'],
-#     "yz_vel": ["x2", "x3", 'vx2','vx3'],
-# }
-
 arr_type_key = {
     "e": "1D cell edge coordinate arrays [x, y, z]",
     "m": "1D cell midpoint coordinate arrays [x, y, z]",
@@ -52,6 +40,39 @@ arr_type_key = {
     "nc": "3D cell edge arrays [x, y, z]",
     "cc": "3D cell midpoint arrays [x, y, z]"
     }
+
+def profiles(arr_type=None):
+
+    coord_prefixes = {
+        "e":  ["ex",  "ey",  "ez"],
+        "m":  ["mx",  "my",  "mz"],
+        "d":  ["dx",  "dy",  "dz"],
+        # "x":  ["x1",  "x2",  "x3"],
+        "nc": ["ncx", "ncy", "ncz"],
+        "cc": ["ccx", "ccy", "ccz"],
+        None: ["x1",  "x2",  "x3"],  # default if None
+    }
+
+    # Get the correct coordinate labels, defaults to mx ...
+    coords = coord_prefixes.get(arr_type, ["x1", "x2", "x3"])
+    x, y, z = coords
+
+    profiles = {
+        "all": [x, y, z, "rho", "prs", "vx1", "vx2", "vx3",'tr1', "sim_time"],
+
+        "grid_time": [x,y,z,"sim_time"],
+        "grid_tracer": [x,y,z,"tr1"],
+
+        "xy_rho_prs": [x, y, "rho", "prs"],
+        "xz_rho_prs": [x, z, "rho", "prs"],
+        "yz_rho_prs": [y, z, "rho", "prs"],
+
+        "xy_vel":     [x, y, "vx1", "vx2"],
+        "xz_vel":     [x, z, "vx1", "vx3"],
+        "yz_vel":     [y, z, "vx2", "vx3"],
+    }
+    returns = {"profiles":profiles,"coord_prefixes":coord_prefixes}
+    return returns
 
 #TODO env_var or config file?
 def get_start_dir(origin_dir,**kwargs):
@@ -76,35 +97,6 @@ def get_start_dir(origin_dir,**kwargs):
         start_dir = new_dir
         warnings.append(f"{pcolours.WARNING}{start_dir} (Missing environment variable PLUTONLIB_START_DIR)")
     return {"start_dir":start_dir,"warnings":warnings}
-
-def profiles2(arr_type=None):
-
-    coord_prefixes = {
-        "e":  ["ex",  "ey",  "ez"],
-        "m":  ["mx",  "my",  "mz"],
-        "d":  ["dx",  "dy",  "dz"],
-        # "x":  ["x1",  "x2",  "x3"],
-        "nc": ["ncx", "ncy", "ncz"],
-        "cc": ["ccx", "ccy", "ccz"],
-        None: ["x1",  "x2",  "x3"],  # default if None
-    }
-
-    # Get the correct coordinate labels, defaults to mx ...
-    coords = coord_prefixes.get(arr_type, ["x1", "x2", "x3"])
-    x, y, z = coords
-
-    profiles = {
-        "all": [x, y, z, "rho", "prs", "vx1", "vx2", "vx3", "sim_time"],
-        "grid_time": [x,y,z,"sim_time"],
-        "xy_rho_prs": [x, y, "rho", "prs"],
-        "xz_rho_prs": [x, z, "rho", "prs"],
-        "yz_rho_prs": [y, z, "rho", "prs"],
-        "xy_vel":     [x, y, "vx1", "vx2"],
-        "xz_vel":     [x, z, "vx1", "vx3"],
-        "yz_vel":     [y, z, "vx2", "vx3"],
-    }
-    returns = {"profiles":profiles,"coord_prefixes":coord_prefixes}
-    return returns
 
 coord_systems = {
     "CYLINDRICAL": ['r','z','theta'],
@@ -186,6 +178,7 @@ def get_pluto_units(sim_coord,ini_file):
         "vx1": {"code_uv": code_unit_values["vx1"], "usr_uv": usr_unit_values["vx1"], "var_name": f"{sel_coords[0]}_Velocity"},
         "vx2": {"code_uv": code_unit_values["vx2"], "usr_uv": usr_unit_values["vx2"], "var_name": f"{sel_coords[1]}_Velocity"},
         "vx3": {"code_uv": code_unit_values["vx3"], "usr_uv": usr_unit_values["vx3"], "var_name": f"{sel_coords[2]}_Velocity"},
+        "tr1" : {"code_uv": None, "usr_uv": None, "var_name": "Tracer_1"},
         "sim_time": {"code_uv": code_unit_values["sim_time"], "usr_uv": usr_unit_values["sim_time"], "var_name": "Time"},
         "ini_file": ini_file,
     }
@@ -205,12 +198,19 @@ def code_to_usr_units(var_name,raw_data = None, self = 0,ini_file = None):
     np.asarray(raw_data) if np.any(raw_data) and not isinstance(raw_data,np.ndarray) else raw_data #calc only works if raw_data is numpy array 
 
     #convert raw_data
-    conv_to_code_units = (raw_data*code_uv)
-    conv_data_cuv = conv_to_code_units.value #equiv to arrays in cgs units
-    conv_data_uuv = conv_to_code_units.to(usr_uv).value #equiv to arrays in si units
+    if code_uv is None or usr_uv is None:  #skips if it doesn't need converting
+        conv_to_code_units = raw_data
+        conv_data_cuv = raw_data  #equiv to arrays in cgs units
+        conv_data_uuv = raw_data
+        uv_usr = None
+    else:
+        conv_to_code_units = (raw_data*code_uv)  
+        conv_data_cuv = conv_to_code_units.value  #equiv to arrays in cgs units
+        conv_data_uuv = conv_to_code_units.to(usr_uv).value #equiv to arrays in si units
+        uv_usr = (1*code_uv).to(usr_uv).value
 
     returns = {
-        "uv_usr":(1*code_uv).to(usr_uv).value, #like a scale factor??
+        "uv_usr":uv_usr, #like a scale factor??
         "conv_data_cuv":conv_data_cuv, 
         "conv_data_uuv":conv_data_uuv 
     }
